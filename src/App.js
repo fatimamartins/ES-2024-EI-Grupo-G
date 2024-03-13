312d35d9f8987271faa3801f9d2650e440d527d2
Fatima Martins <fatima.l.coelho.martins@gmail.com>
2024-03-13 11:40:55 +0000

    fetch csv remotamente (exemplo: github)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import * as React from 'react';
-import { Button, Container } from '@mui/material';
-import TableDemo from './TableDemo';
+import "./App.css";
+import React, { useState } from "react";
+import { Button, Container } from "@mui/material";
+import TableDemo from "./TableDemo";
+import RemoteFile from "./RemoteFile";
 
 function App() {
+  const [horario, setHorario] = useState([]);
+
   return (
     <Container>
       <Button variant="contained">Hello world</Button>
+      <RemoteFile setHorario={setHorario} />
       <TableDemo />
       <p>Hello</p>
     </Container>
---
+++ src/RemoteFile.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Papa from 'papaparse'
+import { Button, Input, Stack } from '@mui/material'
+
+export default function RemoteFile({ setHorario }) {
+    const [fileName, setFileName] = useState('')
+
+    const handleFileChange = (event) => {
+        setFileName(event.target.value)
+    }
+
+    const handleFileUpload = async () => {
+        try {
+            // https://raw.githubusercontent.com/fatimamartins/ES-2024-EI-Grupo-G/f-import_csv/public/HorarioDeExemplo.csv
+            const response = await fetch(fileName)
+            if (!response.ok) {
+                throw new Error('Failed to fetch CSV')
+            }
+            const csvText = await response.text()
+            const result = Papa.parse(csvText, { header: true })
+            setHorario(result.data)
+        } catch (error) {
+            console.error('Error fetching CSV:', error)
+        }
+    }
+
+    return (
+        <Stack direction={'row'} mt={4}>
+            <Input type="text" value={fileName} onChange={handleFileChange} placeholder="CSV File URL" />
+            <Button variant="contained" onClick={handleFileUpload}>
+                Upload
+            </Button>
+        </Stack>
+    )
+}
