e3a576059fa55b906ec6a4a3b4f9eefd39820aec
tiagooliveira24 <160745402+tiagooliveira24@users.noreply.github.com>
2024-03-18 16:50:28 +0000

    tests_v4

--- src/Tests/CsvReader.test.js
+++ src/Tests/CsvReader.test.js
@@ -18,6 +18,30 @@ describe('CsvReader', () => {
         expect(input).toHaveStyle({ marginLeft: '-83px' })
     })
 
+    it('should pass the correct file name to setData function', () => {
+        const setDataMock = jest.fn() // Mocking setData function
+        const id = 'fileInput' // Mocking id for input element
+
+        // Render the CsvReader component with mock props
+        const { getByLabelText } = render(<CsvReader setData={setDataMock} id={id} />)
+
+        // Get the file input element
+        const fileInput = getByLabelText('Escolher ficheiro')
+
+        // Create a sample file
+        const fileName = 'example.csv'
+        const file = new File(['header1,header2\nvalue1,value2'], fileName, { type: 'text/csv' })
+
+        // Simulate file selection and loading
+        fireEvent.change(fileInput, {
+            target: {
+                files: [file],
+            },
+        })
+
+        expect(file.name).toEqual(fileName)
+    })
+
     it('does not call setData when file is empty', () => {
         const mockSetData = jest.fn()
         const { getByLabelText } = render(<CsvReader setData={mockSetData} id="csv-input" />)
--- src/Tests/utils.test.js
+++ src/Tests/utils.test.js
@@ -1,4 +1,11 @@
-import { addWeekNumber, addSemesterWeekNumber, parseDate, sortDate } from '../utils'
+import {
+    addWeekNumber,
+    addSemesterWeekNumber,
+    parseDate,
+    sortDate,
+    validateRoomAvailability,
+    sortWeekDays,
+} from '../utils'
 describe('addWeekNumber', () => {
     it('adiciona corretamente o número da semana à data da aula', () => {
         const defaultData = [
@@ -55,5 +62,60 @@ describe('sortDate', () => {
         expect(result).toBeLessThan(0) // date1 deve vir antes de date2
     })
 
+    test('sorts dates in chronological order', () => {
+        const dates = ['01/01/2024', '02/01/2024', '03/01/2024', '04/01/2024']
+        const shuffledDates = ['03/01/2024', '01/01/2024', '04/01/2024', '02/01/2024']
+        expect(shuffledDates.sort(sortDate)).toEqual(dates)
+    })
+
+    test('handles dates with same value gracefully', () => {
+        const dates = ['01/01/2024', '01/01/2024', '02/01/2024', '03/01/2024']
+        const sameDates = ['02/01/2024', '01/01/2024', '03/01/2024', '01/01/2024']
+        expect(sameDates.sort(sortDate)).toEqual(dates)
+    })
+
     // Adicione mais testes conforme necessário para cobrir outros casos de uso
+})
+
+describe('validateRoomAvailability', () => {
+    const sampleSchedule = [
+        {
+            'Sala atribuída à aula': 'Sala A',
+            'Data da aula': '01/01/2024',
+            'Hora início da aula': '09:00:00',
+            'Hora fim da aula': '10:30:00',
+        },
+        {
+            'Sala atribuída à aula': 'Sala B',
+            'Data da aula': '01/01/2024',
+            'Hora início da aula': '11:00:00',
+            'Hora fim da aula': '12:30:00',
+        },
+        {
+            'Sala atribuída à aula': 'Sala A',
+            'Data da aula': '02/01/2024',
+            'Hora início da aula': '09:00:00',
+            'Hora fim da aula': '10:30:00',
+        },
+    ]
+
+    test('room is available at specified date and hour', () => {
+        expect(
+            validateRoomAvailability('Sala A', new Date('2024-01-01'), new Date('2024-01-01T10:00:00'), sampleSchedule)
+        ).toBe(false)
+    })
+})
+
+describe('sortWeekDays', () => {
+    test('sorts days in order (Mon to Sun)', () => {
+        const sortedDays = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']
+        const shuffledDays = ['Ter', 'Dom', 'Qui', 'Seg', 'Qua', 'Sáb', 'Sex']
+        expect(shuffledDays.sort(sortWeekDays)).toEqual(sortedDays)
+    })
+
+    test('sorts days with repeated days', () => {
+        const sortedDays = ['Seg', 'Seg', 'Ter', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']
+        const shuffledDays = ['Dom', 'Seg', 'Ter', 'Qui', 'Qua', 'Seg', 'Sáb', 'Ter', 'Sex']
+        expect(shuffledDays.sort(sortWeekDays)).toEqual(sortedDays)
+    })
 })
